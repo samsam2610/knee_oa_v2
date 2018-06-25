8ad85b8aa07e6f7a45e2b7c65e9216166b01e721
Sam Tran <quocsam93@gmail.com>
2018-06-25 15:29:24 -0400

    First commit

---
+++ filter.py
@@ -0,0 +1,31 @@
+from scipy.fftpack import fft, fftshift
+from scipy.signal import butter, lfilter, hilbert
+
+def butter_bandpass(lowcut, highcut, fs, order):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def butter_lowpass(cutOff, fs, order=4):
+    nyq = 0.5 * fs
+    normalCutoff = cutOff / nyq
+    b, a = butter(order, normalCutoff, btype='low', analog = True)
+    return b, a
+
+def butter_lowpass_filter(data, cutOff, fs, order=4):
+    b, a = butter_lowpass(cutOff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def frequency_domain(data,nfft):
+    freq_data = fft(data, nfft)
+    freq_data = fftshift(freq_data)
+    return freq_data
